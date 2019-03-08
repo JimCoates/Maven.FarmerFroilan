a47895ad992a6ea0d68ea1bc593a2878d402f100
James Coates <jamescoates@zipcoders-MacBook-Pro.local>
2019-03-08 18:42:02 -0500

    Added Tests for Farm

--- src/test/java/com/zipcodewilmington/froilansfarm/Storage/FarmTest.java
+++ src/test/java/com/zipcodewilmington/froilansfarm/Storage/FarmTest.java
@@ -1,5 +1,6 @@
 package com.zipcodewilmington.froilansfarm.Storage;
 
+import com.sun.corba.se.impl.interceptors.SlotTableStack;
 import com.zipcodewilmington.froilansfarm.Animal.AnimalFactory;
 import com.zipcodewilmington.froilansfarm.Vehicle.CropDuster;
 import com.zipcodewilmington.froilansfarm.Vehicle.Tractor;
@@ -10,18 +11,45 @@ public class FarmTest {
 
 
     @Test
-    public void farmConstructorTest(){
+    public void farmConstructorVariablesTest(){
         Farm farm = new FarmBuilder()
-                .setBetty(new CropDuster())
-                .setFarmHouse(new FarmHouse())
                 .setNumberCoops(1)
                 .setNumberFields(4)
-                .setTractor(new Tractor())
                 .setNumberStables(5)
                 .createFarm();
         FarmHouse farmhouse = farm.getFarmHouse();
+        Integer expectedFields = 4;
+        Integer expectedCoops = 1;
+        Integer expectedStables = 5;
+
+
+        Integer actualFields = farm.getFieldList();
+        Integer actualStables = farm.getHorseStableList();
+        Integer actualCoops = farm.getChickenCoopList();
 
         Assert.assertNotNull(farmhouse);
+        Assert.assertEquals(expectedFields, actualFields);
+        Assert.assertEquals(expectedCoops,actualCoops);
+        Assert.assertEquals(expectedStables,actualStables);
+    }
 
+    @Test
+    public void farmConstructorDefaultTest(){
+        Farm farm = new Farm();
+
+        FarmHouse farmhouse = farm.getFarmHouse();
+        Integer expectedFields = 1;
+        Integer expectedCoops = 4;
+        Integer expectedStables = 3;
+
+
+        Integer actualFields = farm.getFieldList();
+        Integer actualStables = farm.getHorseStableList();
+        Integer actualCoops = farm.getChickenCoopList();
+
+        Assert.assertNotNull(farmhouse);
+        Assert.assertEquals(expectedFields, actualFields);
+        Assert.assertEquals(expectedCoops,actualCoops);
+        Assert.assertEquals(expectedStables,actualStables);
     }
 }
