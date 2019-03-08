7d95d48ad91dfa647886822ffab00f81b87f5436
Michael Krohn <michaelkrohn@MacBook-Pro-8.localdomain>
2019-03-08 18:44:04 -0500

    idk

--- src/main/java/com/zipcodewilmington/froilansfarm/Storage/Farm.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/Storage/Farm.java
@@ -64,4 +64,10 @@ public class Farm {
     public Integer getHorseStableList() {
         return horseStableList.size();
     }
+
+    public Farm returnThisFarm(){
+        return this;
+    }
+
+    
 }
